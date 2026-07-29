681de403f7d570c12e369b22902d15e7a7f0d5b6
sadiq-bilal <sadiqbilal2018@gmail.com>
2026-07-29 21:35:56 +0530

    updated Day 10 and added README.md file

--- Day-10/arrayRotation(K).js
+++ Day-10/arrayRotation(K).js
@@ -1,24 +1,33 @@
-// Left Rotate Array by K posiions
+//Right Rotate Array by K positions
 
+let arr = [1, 2, 3, 4, 5, 6];  
+let k = 350;
+k = k %  arr.length; //8 % 6 = 2
 
-
-let arr = [1, 2, 3, 4, 5]; // 5,4,1,2,3
-let last = arr[arr.length - 1];
-         // 0
-for (let i = arr.length-1; i >0;i--){
-        // 2      // 1 
-    arr[i] = arr[i-1];
-}
-
- arr[0]= last;
-
-console.log(arr);
-
-// let arr = [1, 2, 3, 4, 5];  // 3,4,5,1,2
-
-function leftRotate (arr,k){
-    k = k % arr.length;
-    for (let i = 0; i < k; i++){
-        
+for(let i = 0; i < k; i++){   // O(k)
+    let last = arr[arr.length-1];
+    for(let j = arr.length-1; j > 0; j--){ // O (n)
+        arr[j] = arr[j - 1];
     }
+    arr[0] = last;
 }
+console.log(arr);//[5,6,1,2,3,4] O(n*k) 
+
+
+//Right Rotate Array by K positions
+
+// let arr = [1, 2, 3, 4, 5, 6];  
+
+// let k = 350;
+// k = k %  arr.length; //8 % 6 = 2
+//                    //2
+// for (let i = 0; i < k; i++){ 
+//     let first = arr[0]; // 2
+                
+//     for (let j = 0; j < arr.length-1;j++){ // [2,3,4,5,1]
+//         // 4    // 4
+//         arr[j] = arr[j+1]; // [3,4,5,1,1]
+//     }
+//     arr[arr.length-1] = first; // [3,4,5,1,2]
+// }
+// console.log(arr);
