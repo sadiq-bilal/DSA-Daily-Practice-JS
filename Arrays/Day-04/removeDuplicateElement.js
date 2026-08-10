4a3d16ee7d9a7d87b69f74e6f5d428c626dbffa1
sadiq-bilal <sadiqbilal2018@gmail.com>
2026-08-10 21:55:50 +0530

    Refactor removeDuplicateElement.js with new method
    Updated comments for clarity and added a new method to remove duplicates.

--- Arrays/Day-04/removeDuplicateElement.js
+++ Arrays/Day-04/removeDuplicateElement.js
@@ -1,5 +1,5 @@
-// Remove Duplicate Elements
-
+// Remove Duplicate Elements from an sorted array
+// Method 01
 let dupNums = [1, 2, 2, 3, 4, 4, 5, 6, 6, 6, 7];
 let originalNums = [];
 
@@ -11,3 +11,19 @@ for (let i =1; i<= dupNums.length -1 ;i++){
     }
 }
 console.log(originalNums);
+
+// Method 03
+let arr = [1,2,3,3,3,4,4,5];
+
+let j = 0;// 5*  j-val = 4
+         // 7   i-val = 5
+for (let i = 1; i < arr.length; i++) {
+    
+    if (arr[i] !== arr[j]) {
+        j++;
+       // 5
+        arr[j] = arr[i];  //[1,2,3,4,5,4,4,5]
+    }
+}
+
+console.log(arr.slice(0, j + 1));
